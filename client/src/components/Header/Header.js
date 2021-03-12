690ef4f988bb9aad7153e98f984f8480578e9bb3
MH4454 <68453774+MH4454@users.noreply.github.com>
2021-03-12 15:27:16 -0500

    Added Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,8 +3,10 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="jumbotron">
+          <h1 className="display-4 text-center">(React) Google Books Search</h1>
+          <h2 className="lead text-center">Search for and Save Books of Interest</h2>
+
         </div>
     )
 }
