bccfa5065d95379274fba8564f8d307df6a6c6df
MH4454 <68453774+MH4454@users.noreply.github.com>
2021-03-12 14:59:39 -0500

    Added Bootstrap with base navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar"
 
 
 function App() {
   return (
 <>
 <Router>
-  
+  <Navbar />
   <Route />
 </Router>
 </>
