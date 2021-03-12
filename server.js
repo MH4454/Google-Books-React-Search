ad13016a9870475ccb0ded91f85882ac6f3370f2
MH4454 <68453774+MH4454@users.noreply.github.com>
2021-03-12 18:16:15 -0500

    Made a connection to the DB and added index model

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const connectDB = require("./database/connection");
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+connectDB();
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
