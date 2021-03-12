79ebd3c0d4d443dc0fb5d23f22d3ae89531b3329
MH4454 <68453774+MH4454@users.noreply.github.com>
2021-03-12 15:59:13 -0500

    Added the search bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar"
 import Header from "./components/Header/Header"
+import Searchbar from "./components/SearchBar/Searchbar"
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
 <Router>
   <Navbar />
   <Header />
-  <Route />
+  <Route exact path="/" component={Searchbar}/>
 </Router>
 </>
   );
--- client/src/components/SearchBar/Searchbar.js
+++ client/src/components/SearchBar/Searchbar.js
@@ -3,9 +3,24 @@ import "./Searchbar.css"
 
 const Searchbar = () => {
     return (
-        <div>
-            
+        <>
+    <div className="container">
+        <div className="row">
+            <div className="col"><h3>Book Search</h3></div>
         </div>
+        <div className="row mt-4">
+            <div className="col">Book:</div>
+        </div>
+        <div className="row">
+            <div className="col">
+            <input className="form-control" type="text" placeholder="Default input"></input>
+            </div>
+        </div>
+        <div className="row mt-4">
+            <div className="col d-flex justify-content-end"><input className="btn text-white" type="submit" value="Submit"></input></div>
+        </div>
+    </div>
+        </>
     )
 }
 
